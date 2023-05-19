87aec6362e7acf3797160f33472e975177c45509
Miguel Cabrera <macabrerad06@gmail.com>
2023-05-19 10:18:50 -0500

    Reestructuracion

--- Parcial 1/Clase 19-05/Trabajo en Grupo/Convertidor.java
+++ Parcial 1/Clase 19-05/Trabajo en Grupo/Convertidor.java
@@ -1,8 +1,26 @@
+import java.util.Stack;
 public class Convertidor {
-    
-    
+    public static void decimalABinario(int numero) {
 
+        Stack<Integer> pila = new Stack<>();
 
+        while (numero > 0) {
+            int residuo = numero % 2;
+            pila.push(residuo);
+            numero /= 2;
+        }
 
+        System.out.print("El número binario es: ");
 
+        while (!pila.empty()) {
+                System.out.print(pila.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int decimal = 27;
+        
+        decimalABinario(decimal);
+    }
 }
