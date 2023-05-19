e027bd4e9966639fa1beada65b8404ff589ee21e
Miguel Cabrera <macabrerad06@gmail.com>
2023-05-19 10:27:41 -0500

    Comentando

--- Parcial 1/Clase 19-05/Trabajo en Grupo/Convertidor.java
+++ Parcial 1/Clase 19-05/Trabajo en Grupo/Convertidor.java
@@ -1,29 +1,35 @@
 import java.util.Stack;
 
-
 public class Convertidor {
 
     public static void decimalABinario(int numero) {
-
+        // Se crea una instancia de la clase Stack para almacenar los residuos
         Stack<Integer> pila = new Stack<>();
 
+        // Se realiza la conversión del número decimal a binario mediante divisiones sucesivas
         while (numero > 0) {
             int residuo = numero % 2;
+            // El residuo se agrega a la pila
             pila.push(residuo);
+            // El número se divide por 2 para continuar con la siguiente división
             numero /= 2;
         }
 
+        // Se imprime el mensaje inicial
         System.out.print("El número binario es: ");
 
+        // Se desapila y se imprime cada elemento hasta que la pila esté vacía
         while (!pila.empty()) {
-                System.out.print(pila.pop());
+            System.out.print(pila.pop());
         }
     }
 
     public static void main(String[] args) {
-
+        // Se define el número decimal a convertir
         int decimal = 27;
 
+        // Se llama al método decimalABinario pasando el número decimal como argumento
         decimalABinario(decimal);
     }
 }
+
